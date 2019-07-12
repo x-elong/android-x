a0ad58dc63d306d5207d05d4334e292716e550fd
钟正航 <1156864734@qq.com>
2019-07-12 15:34:39 +0800

    ahaha

---
+++ app/src/main/java/com/example/eletronicengineer/activity/ProjectPlateActivity.kt
@@ -0,0 +1,13 @@
+package com.example.eletronicengineer.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eletronicengineer.R
+
+class ProjectPlateActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_project_plate)
+    }
+}
